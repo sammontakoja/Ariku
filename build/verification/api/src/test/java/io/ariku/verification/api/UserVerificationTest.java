0a896a45c6be5a33eb766c181cb4bcca786cd555
sammontakoja <sammontakoja@protonmail.com>
2016-10-25 16:35:33 +0300

    Added test annotations. Added junit tools so tests can run with parallel runner.

--- build/verification/api/src/test/java/io/ariku/verification/api/UserVerificationTest.java
+++ build/verification/api/src/test/java/io/ariku/verification/api/UserVerificationTest.java
@@ -1,26 +1,32 @@
 package io.ariku.verification.api;
 
+import com.googlecode.junittoolbox.ParallelRunner;
 import io.ariku.util.data.User;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Ari Aaltonen
  */
+@RunWith(ParallelRunner.class)
 public class UserVerificationTest {
 
+    @Test
     public void user_can_signUp() {
         new UserVerification().signUp(new User());
     }
 
+    @Test
     public void user_can_verifySignUp() {
         new UserVerification().verifySignUp();
     }
 
+    @Test
     public void user_can_ogin() {
         new UserVerification().login();
     }
 
+    @Test
     public void user_can_logout() {
         new UserVerification().logout();
     }
