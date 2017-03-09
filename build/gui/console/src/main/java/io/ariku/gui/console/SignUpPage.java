360a0f6fafb88561676ae96fd5036e875b57774d
sammontakoja <sammontakoja@protonmail.com>
2017-03-09 14:18:35 +0200

    Simplified form logic

--- build/gui/console/src/main/java/io/ariku/gui/console/SignUpPage.java
+++ build/gui/console/src/main/java/io/ariku/gui/console/SignUpPage.java
@@ -4,8 +4,6 @@ import com.googlecode.lanterna.gui2.BasicWindow;
 import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.TextBox;
-import io.ariku.verification.SignUpRequest;
-import io.ariku.verification.UserVerificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +23,12 @@ public class SignUpPage {
         TextBox username = new TextBox();
         username.addTo(panel);
 
-        Button okButton = new Button("OK", () -> {
+        panel.addComponent(new Button("OK", () -> {
             String response = restClient.signUpRequest(username.getText());
             logger.debug("SignUpRequest with username:{} {}", username, response);
-        });
-        okButton.addTo(panel);
+        }));
 
-        Button exitButton = new Button("Exit", () -> userVerificationMenu.draw(window));
-        exitButton.addTo(panel);
+        panel.addComponent(new Button("Exit", () -> userVerificationMenu.draw(window)));
 
         window.setComponent(panel);
     }
